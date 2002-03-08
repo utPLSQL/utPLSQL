e595f2b40e4e941d8aca123805e0f2b0713ca179
chrisrimmer <chrisrimmer@ee310c5f-e4e9-472c-a0a6-434e6ab0a9a2>
2002-03-08 13:54:53 +0000

    Initial Load

---
+++ documentation/src/build_docs.pl
@@ -0,0 +1,132 @@
+#!/usr/bin/perl -w
+#
+# This perl script builds the documentation for utPLSQL
+# $Id$
+#
+
+use strict;
+
+#The directory the output goes into
+my $OUTDIR = "..";
+
+#Holds the map
+my @map;
+
+#Holds the top navigation string
+my $nav = "";
+
+#Read the map file
+open MAP, "map.txt" or die "Cannot open map.txt";
+my ($filename,$desc,$section);
+while (<MAP>){
+
+	#Ignore lines starting with #, or empty
+	if (not /^#/ and /\S/){
+		chomp;
+
+		#Split on comma
+		($filename, $desc) = split /,/;
+
+		$section = 0;
+
+		#Add entries ending in * to navigation bar
+		if ($desc =~ s/\*$//){
+			$nav .= " | " if $nav;
+			$nav .= "<A href=\"$filename\">$desc</A>\n";
+			$section = 1;
+		}
+		push @map, [$filename, $desc, $section];
+	}
+}
+close MAP;
+
+#Add the document map
+push @map, ["map.html", "Document Map", 1];
+$nav = "[ $nav | <A href=\"map.html\">Document Map</A> ]";
+
+#Holds the copyright string
+my $copyright;
+
+#Read the authors file
+open AUTHORS, "authors.txt" or die "Cannot open authors.txt";
+my ($name, $email);
+while (<AUTHORS>){
+
+	#Ignore lines starting with #, or empty
+	if (not /^#/ and /\S/){
+		chomp;
+		($name, $email) = split /,/;
+		$copyright .= ', ' if $copyright;
+		$copyright .= "<A href=\"mailto:$email\">$name<A>";
+	}
+}
+close AUTHORS;
+
+#Put together the rest of the copyright notice
+$copyright = "Copyright (C) 2000-".(((gmtime(time))[5])+1900)." $copyright All rights reserved";
+
+my $logo = '<div class="purple_bar"><a href="index.html"><img src="utplsql.jpg" border=0></a></div>';
+
+#Now build the documentation
+my $index;
+my $body;
+my $nextprev;
+foreach $index (0..$#map){
+
+	if ($index != 0){
+		$nextprev = '<A href="'.($map[$index-1]->[0]).'">&lt; Previous Section: '.($map[$index-1]->[1]).'</A>';
+	} else {
+		$nextprev = '';
+	}
+	if ($index != $#map){
+		$nextprev .= ' | ' if $nextprev;
+		$nextprev .= '<A href="'.($map[$index+1]->[0]).'">Next Section: '.($map[$index+1]->[1]).' &gt;</A>';
+	}	
+
+	$body = 0;
+	open OUTPUT, ">$OUTDIR/$map[$index]->[0]" or die "Cannot open $OUTDIR/$map[$index]->[0]";
+
+	if ($index != $#map){
+		system("./clean_html.pl $map[$index]->[0] > $map[$index]->[0].clean");
+		open INPUT, "$map[$index]->[0].clean" or die "Cannot open $map[$index]->[0].clean";
+	}
+
+	print OUTPUT "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n";
+	print OUTPUT "<html><head>\n";
+	print OUTPUT "<title>$map[$index]->[1]</title>\n";
+	print OUTPUT "<link rel=\"stylesheet\" href=\"utplsql.css\" content=\"text/css\">";
+	print OUTPUT "</head><body>\n";
+	print OUTPUT "$logo\n";
+	print OUTPUT "<p>$nav</p>\n";
+	print OUTPUT "<p>$nextprev</p>\n";
+
+	#Either print the body, or construct it for the document map 
+	if ($index != $#map){
+		while (<INPUT>){
+			$body = 1 if /^\s*<!-- Begin utPLSQL Body -->\s*$/;
+			print OUTPUT $_ if $body;
+			$body = 0 if /^\s*<!-- End utPLSQL Body -->\s*$/i;
+		}
+		close INPUT;
+		unlink("$map[$index]->[0].clean");
+	} else {
+		print OUTPUT "<h1>Document Map</h1>\n";
+		foreach (@map){
+			if ($_->[2]){
+				print OUTPUT "<b>";
+			} else {
+				print OUTPUT "&nbsp;&nbsp;";
+			}
+			print OUTPUT "<A href=\"$_->[0]\">$_->[1]</A><br>";
+			print OUTPUT "</b>" if $_->[2];
+			print OUTPUT "\n";
+		}
+	}
+
+	print OUTPUT "<p>$nextprev</p>\n";
+	print OUTPUT "$logo\n";
+	print OUTPUT "<p class=\"copyright\">$copyright</p>\n";
+	print OUTPUT "</body></html>";	
+	close OUTPUT;
+}
+
