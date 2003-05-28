15014279ebe29300c397a5a77d4de7f345412e95
chrisrimmer <chrisrimmer@ee310c5f-e4e9-472c-a0a6-434e6ab0a9a2>
2003-05-28 08:28:09 +0000

    More "discriminating" version of uninstall script

--- utplsql/trunk/source/ut_i_uninstall.sql
+++ utplsql/trunk/source/ut_i_uninstall.sql
@@ -8,102 +8,127 @@ SET DEFINE ON
 TTITLE OFF
 SET SERVEROUTPUT ON SIZE 1000000 FORMAT WRAPPED
 
-DEFINE uscript='ut_i_spool_temp.sql'
-
-COLUMN col NOPRINT NEW_VALUE ut_owner
-SELECT USER col FROM DUAL;
-
-DEFINE line1='-------------------------------------------------------------'
-DEFINE line2='============================================================='
-DEFINE finished='.                            Finished'
-
-
-SPOOL &uscript
-
-REM synonyms: --------------------------------------------------------------
-
-COLUMN col NOPRINT NEW_VALUE fine
-SELECT COUNT (PRIVILEGE) col
-  FROM session_privs
- WHERE PRIVILEGE = 'DROP PUBLIC SYNONYM';
-
-SELECT DECODE (&fine
-             , 0, ''
-             , 'PROMPT Dropping &UT public synonyms...'
-              )
-  FROM DUAL;
-
-SELECT DECODE (&fine
-             , 0, ''
-             , 'drop public synonym ' || o1.object_name || ';'
-              )
-  FROM all_objects o1, all_objects o2
- WHERE o1.owner = 'PUBLIC'
-   AND o1.object_type = 'SYNONYM'
-   AND o1.object_name = o2.object_name
-   AND o1.object_name LIKE 'UT%'
-   AND o2.object_type IN ('PACKAGE', 'TABLE', 'VIEW','SEQUENCE')
-   AND o2.owner = '&ut_owner';
-
-SELECT DECODE (&fine, 0, '', 'PROMPT &finished')
-  FROM DUAL;
-
-REM tables: ----------------------------------------------------------------
-
-SELECT 'PROMPT Dropping &UT tables...'
-  FROM DUAL;
-
-SELECT 'drop table ' || object_name || ' cascade constraints;'
-  FROM all_objects
- WHERE owner = '&ut_owner'
-   AND object_name LIKE 'UT%'
-   AND object_type = 'TABLE';
-
-SELECT 'PROMPT &finished'
-  FROM DUAL;
-
-REM views: ----------------------------------------------------------------
-
-SELECT 'PROMPT Dropping &UT views...'
-  FROM DUAL;
-
-SELECT 'drop view ' || object_name || ' cascade constraints;'
-  FROM all_objects
- WHERE owner = '&ut_owner'
-   AND object_name LIKE 'UT%'
-   AND object_type = 'VIEW';
-
-SELECT 'PROMPT &finished'
-  FROM DUAL;
-
-REM sequences: --------------------------------------------------------------
-
-SELECT 'PROMPT Dropping &UT sequences...'
-  FROM DUAL;
-
-SELECT 'drop sequence ' || object_name || ';'
-  FROM all_objects
- WHERE owner = '&ut_owner'
-   AND object_name LIKE 'UT%'
-   AND object_type = 'SEQUENCE';
-
-SELECT 'PROMPT &finished'
-  FROM DUAL;
-
-REM sequences: --------------------------------------------------------------
-
-SELECT 'PROMPT Dropping &UT packages...'
-  FROM DUAL;
-
-SELECT 'drop package ' || object_name || ';'
-  FROM all_objects
- WHERE owner = '&ut_owner'
-   AND object_name LIKE 'UT%'
-   AND object_type = 'PACKAGE';
-
-SELECT 'PROMPT &finished'
-  FROM DUAL;
-
+drop package UTAEQ;
+drop package UTASSERT2;
+drop package UTASSERT;
+drop package UTCONFIG;
+drop package UTGEN;
+drop package UTOUTCOME;
+drop package UTOUTPUT;
+drop package UTPACKAGE;
+drop package UTPLSQL2;
+drop package UTPLSQL;
+drop package UTPLSQL_UTIL;
+drop package UTRECEQ;
+drop package UTRERROR;
+drop package UTRESULT2;
+drop package UTRESULT;
+drop package UTROUTCOME;
+drop package UTRSUITE;
+drop package UTRTESTCASE;
+drop package UTRUNITTEST;
+drop package UTRUTP;
+drop package UTSUITE;
+drop package UTSUITEUTP;
+drop package UTTEST;
+drop package UTTESTCASE;
+drop package UTTESTPREP;
+drop package UTUNITTEST;
+drop package UTUTP;
+drop package UTVVALUE;
+drop public synonym UTAEQ;
+drop public synonym UTASSERT2;
+drop public synonym UTASSERT;
+drop public synonym UTA_EQ;
+drop public synonym UTCONFIG;
+drop public synonym UTGEN;
+drop public synonym UTOUTCOME;
+drop public synonym UTOUTPUT;
+drop public synonym UTPACKAGE;
+drop public synonym UTPLSQL2;
+drop public synonym UTPLSQL;
+drop public synonym UTPLSQL_UTIL;
+drop public synonym UTRECEQ;
+drop public synonym UTRERROR;
+drop public synonym UTRESULT2;
+drop public synonym UTRESULT;
+drop public synonym UTROUTCOME;
+drop public synonym UTRSUITE;
+drop public synonym UTRTESTCASE;
+drop public synonym UTRUNITTEST;
+drop public synonym UTRUTP;
+drop public synonym UTR_ERROR;
+drop public synonym UTR_OUTCOME;
+drop public synonym UTR_SUITE;
+drop public synonym UTR_TESTCASE;
+drop public synonym UTR_UNITTEST;
+drop public synonym UTR_UTP;
+drop public synonym UTSUITE;
+drop public synonym UTSUITEUTP;
+drop public synonym UTTEST;
+drop public synonym UTTESTCASE;
+drop public synonym UTTESTPREP;
+drop public synonym UTUNITTEST;
+drop public synonym UTUTP;
+drop public synonym UTVVALUE;
+drop public synonym UTV_VALUE;
+drop public synonym UT_ARGUMENT;
+drop public synonym UT_ASSERTION;
+drop public synonym UT_CONFIG;
+drop public synonym UT_DETERMINISTIC;
+drop public synonym UT_DETERMINISTIC_ARG;
+drop public synonym UT_EQ;
+drop public synonym UT_GRID;
+drop public synonym UT_OUTCOME;
+drop public synonym UT_PACKAGE;
+drop public synonym UT_RECEQ;
+drop public synonym UT_RECEQ_PKG;
+drop public synonym UT_SUITE;
+drop public synonym UT_SUITE_UTP;
+drop public synonym UT_TEST;
+drop public synonym UT_TESTCASE;
+drop public synonym UT_TESTPREP;
+drop public synonym UT_UNITTEST;
+drop public synonym UT_UTOUTPUT;
+drop public synonym UT_UTP;
+drop sequence UTA_EQ_SEQ;
+drop sequence UTPLSQL_RUNNUM_SEQ;
+drop sequence UTV_VALUE_SEQ;
+drop sequence UT_ASSERTION_SEQ;
+drop sequence UT_PACKAGE_SEQ;
+drop sequence UT_RECEQ_SEQ;
+drop sequence UT_REFCURSOR_RESULTS_SEQ;
+drop sequence UT_SUITE_SEQ;
+drop sequence UT_TESTCASE_SEQ;
+drop sequence UT_TEST_SEQ;
+drop sequence UT_UNITTEST_SEQ;
+drop sequence UT_UTP_SEQ;
+drop table UTA_EQ cascade constraints;
+drop table UTR_ERROR cascade constraints;
+drop table UTR_OUTCOME cascade constraints;
+drop table UTR_SUITE cascade constraints;
+drop table UTR_TESTCASE cascade constraints;
+drop table UTR_UNITTEST cascade constraints;
+drop table UTR_UTP cascade constraints;
+drop table UTV_VALUE cascade constraints;
+drop table UT_ARGUMENT cascade constraints;
+drop table UT_ASSERTION cascade constraints;
+drop table UT_CONFIG cascade constraints;
+drop table UT_DETERMINISTIC cascade constraints;
+drop table UT_DETERMINISTIC_ARG cascade constraints;
+drop table UT_EQ cascade constraints;
+drop table UT_GRID cascade constraints;
+drop table UT_OUTCOME cascade constraints;
+drop table UT_PACKAGE cascade constraints;
+drop table UT_RECEQ cascade constraints;
+drop table UT_RECEQ_PKG cascade constraints;
+drop table UT_SUITE cascade constraints;
+drop table UT_SUITE_UTP cascade constraints;
+drop table UT_TEST cascade constraints;
+drop table UT_TESTCASE cascade constraints;
+drop table UT_TESTPREP cascade constraints;
+drop table UT_UNITTEST cascade constraints;
+drop table UT_UTP cascade constraints;
 
 SPOOL OFF
 SET TERMOUT ON
