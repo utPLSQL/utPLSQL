7b5ce66aa1fca52ad32841214243fb8c571e1e2f
chrisrimmer <chrisrimmer@ee310c5f-e4e9-472c-a0a6-434e6ab0a9a2>
2003-06-09 16:22:40 +0000

    Minor Tweaks

--- source/ut_i_synonyms.sql
+++ source/ut_i_synonyms.sql
@@ -21,11 +21,12 @@ COLUMN col NOPRINT NEW_VALUE fine
 select count(privilege) col from session_privs where privilege='CREATE PUBLIC SYNONYM';
 
 SPOOL &uscript
-select decode(&fine,0,'','PROMPT Creating synonyms for packages...') from dual;
+select decode(&fine,0,'','PROMPT Creating synonyms ...') from dual;
 select decode(&fine,0,'','create public synonym '||object_name||' for '||object_name||';')
 from all_objects
-where owner='&ut_owner' and object_name like 'UT%' and 
-  object_type in ('PACKAGE','TABLE','VIEW','SEQUENCE');
+where owner='&ut_owner' 
+and object_name like 'UT%' 
+and object_type IN ('PACKAGE', 'TABLE', 'VIEW', 'SEQUENCE');
 select decode(&fine,0,'','PROMPT &finished') from dual;
 
 select decode(&fine,1,'','PROMPT Skipped - user has no rights to create public synonyms') from dual;
