9a514bf3af5af83d01312ff287ce00ce47fe715e
Pazus <pazzzus@gmail.com>
2016-07-24 21:25:39 +0300

    forgotten commit

---
+++ examples/RunAllExamples.sql
@@ -0,0 +1,8 @@
+Clear Screen
+set echo off
+set feedback off
+@@RunExampleComplexSuiteWithCustomDBMSOutputReporter.sql
+@@RunExampleTestSuite.sql
+@@RunExampleTestSuiteWithCustomDBMSOutputReporter.sql
+@@RunExampleTestSuiteWithDBMSOutputReporter.sql
+@@RunExampleTestThroughBaseClass.sql
