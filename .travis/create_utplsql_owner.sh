7c822c6fa717fa1708f6e51a056912a8095543a6
David Pyke <shoelace_822695@hotmail.com>
2016-11-28 15:08:18 +0000

    fix script name

--- .travis/create_utplsql_owner.sh
+++ .travis/create_utplsql_owner.sh
@@ -4,5 +4,5 @@ set -ev
 #create user
 "$ORACLE_HOME/bin/sqlplus" -L -S / AS SYSDBA <<SQL
 set echo off
-@@$(dirname "$(readlink -f "$0")")/create_utplsql_user.sql $UT3_OWNER $UT3_OWNER_PASSWORD $UT3_OWNER_TABLESPACE
+@@$(dirname "$(readlink -f "$0")")/create_utplsql_owner.sql $UT3_OWNER $UT3_OWNER_PASSWORD $UT3_OWNER_TABLESPACE
 SQL
--- .travis/create_utplsql_user.sh
+++ .travis/create_utplsql_user.sh
@@ -4,7 +4,7 @@ set -ev
 #create user
 "$ORACLE_HOME/bin/sqlplus" -L -S / AS SYSDBA <<SQL
 set echo off
-@@$(dirname "$(readlink -f "$0")")/create_utplsql_user.sql $UT3_USER $UT3_USER_PASSWORD $UT3_USER_TABLESPACE
+@@$(dirname "$(readlink -f "$0")")/create_utplsql_owner.sql $UT3_USER $UT3_USER_PASSWORD $UT3_USER_TABLESPACE
 SQL
 #give grants
 "$ORACLE_HOME/bin/sqlplus" -L -S $UT3_OWNER/$UT3_OWNER_PASSWORD <<SQL
