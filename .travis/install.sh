6d82359fc68f9982cd55abd3f333f72a5bd572ad
Jacek Gębal <jgebal@gmail.com>
2017-01-07 16:24:35 +0000

    Disabling debug output - too verbose for CI

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,4 +13,4 @@ cd build
 "$ORACLE_HOME/bin/sqlplus" $UT3_USER/$UT3_PASSWORD @utplsql_style_check.sql
 
 #enable plsql debug
-"$ORACLE_HOME/bin/sqlplus" $UT3_USER/$UT3_PASSWORD @ut_debug_enable.sql
+#"$ORACLE_HOME/bin/sqlplus" $UT3_USER/$UT3_PASSWORD @ut_debug_enable.sql
