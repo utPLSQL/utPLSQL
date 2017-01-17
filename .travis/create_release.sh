947d94eca07d4b425592f079665540af32f77bc0
rlove <rlove@peakbiz.com>
2017-01-17 10:09:31 -0700

    Change permissions on scripts so they can execute under travis build.

--- .travis/create_release.sh
+++ .travis/create_release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Change working directory to script directory
 cd "${0%/*}"
+chmod +x ./build_docs.sh
+chmod +x ./create_release_archive.sh
 ./build_docs.sh
 ./create_release_archive.sh
