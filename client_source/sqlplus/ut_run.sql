5a7c57dc672b52e3f0869e27b28fd9d51bd21371
Jacek Gębal <jgebal@gmail.com>
2017-02-04 19:28:54 +0000

    Added missing license headers.

--- client_source/sqlplus/ut_run.sql
+++ client_source/sqlplus/ut_run.sql
@@ -1,3 +1,20 @@
+/*
+  utPLSQL - Version X.X.X.X
+  Copyright 2016 - 2017 utPLSQL Project
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 /**
   This script is designed to allow invocation of UTPLSQL with multiple reporters.
   It allows saving of outcomes into multiple output files.
