d7d7e6144104dd502b039d687c4c9e02bb19d701
Pazus <pazzzus@gmail.com>
2017-02-26 14:16:49 +0300

    added fixed tests for beforeeach/aftereach

---
+++ tests/ut_test/ut_test.BeforeEachExecuted.sql
@@ -0,0 +1,21 @@
+PROMPT Invoke beforeeach procedure
+
+--Arrange
+declare
+  simple_test ut_test := ut_test(
+    a_object_name            => 'ut_example_tests'
+    ,a_name        => 'ut_passing_test'
+    ,a_before_each_proc_name => 'beforeeach'
+  );
+  listener ut_event_listener := ut_event_listener(ut_reporters());
+begin
+  ut_example_tests.g_number2 := null;
+--Act
+  simple_test.do_execute(listener);
+--Assert
+  if ut_example_tests.g_number2 = 1 then
+    :test_result := ut_utils.tr_success;
+  end if;
+end;
+/
+
