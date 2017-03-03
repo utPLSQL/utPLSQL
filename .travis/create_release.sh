21023a5eff1a0949fcdb6124705df17d3421c2de
Jacek Gębal <jgebal@gmail.com>
2017-03-03 01:04:35 +0000

    Experimenting with building a version from tag and travis build number.

--- .travis/create_release.sh
+++ .travis/create_release.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Change working directory to script directory
 cd "${0%/*}"
+chmod +x ./update_version.sh
 chmod +x ./build_docs.sh
 chmod +x ./create_release_archive.sh
-./build_docs.sh
-./create_release_archive.sh
+
+bash ./update_version.sh
+bash ./build_docs.sh
+bash ./create_release_archive.sh
--- .travis/create_release_archive.sh
+++ .travis/create_release_archive.sh
@@ -8,14 +8,14 @@ rm -rf release
 # Create needed directories
 mkdir -p release/docs/html
 mkdir release/docs/markdown
-mkdir release/source
+
 # Copy files to various directories
 cp -r docs release/docs/markdown
-cp -r site release/docs/html 
+cp -r site release/docs/html
 cp -r client_source release/client_source
 cp -r source release/source
 cp -r examples release/examples
-cp -r readme.md release/ 
+cp -r readme.md release/
 cp -r LICENSE release/
 cp -r authors.md release/
 cp -r CONTRIBUTING.md release/
@@ -23,10 +23,10 @@ cd release
 #Although the $TRAVIS_TAG versions are the only one used.  They are conditional,
 #and we want the process always run to insure we don't have problems with building archive
 #when we finally tag a release
-zip -r ../utPLSQL.zip *
-tar -zcvf ../utPLSQL.tar.gz *
+zip -r -q ../utPLSQL.zip *
+tar -zcf ../utPLSQL.tar.gz *
 #Name of archive will match tag name for a release.
-if [ -n "$TRAVIS_TAG" ]; then 
+if [ -n "$TRAVIS_TAG" ]; then
 cd ..
 cp utPLSQL.zip utPLSQL$TRAVIS_TAG.zip
 cp utPLSQL.tar.gz utPLSQL$TRAVIS_TAG.tar.gz
