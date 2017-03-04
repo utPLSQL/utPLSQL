36c08e5d0b94ddf965fdb0e0213399ef87a2598d
Pazus <pazzzus@gmail.com>
2017-03-04 18:47:54 +0300

    changed update_release script to before_script

--- .travis/create_release.sh
+++ .travis/create_release.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # Change working directory to script directory
 cd "${0%/*}"
-chmod +x ./update_version.sh
 chmod +x ./build_docs.sh
 chmod +x ./create_release_archive.sh
 
-bash ./update_version.sh
 bash ./build_docs.sh
 bash ./create_release_archive.sh
