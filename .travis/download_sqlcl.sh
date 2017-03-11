129afdef1cbd630456db5aabbd4aa900e2e6081d
Vinicius Avellar <amvinicius@gmail.com>
2017-03-11 15:56:35 -0300

    Changes on download tool.

--- .travis/download_sqlcl.sh
+++ .travis/download_sqlcl.sh
@@ -4,7 +4,6 @@ set -e
 cd .travis
 if [ ! -d $HOME/.cache/sqlcl ]; then
     sh download.sh -p sqlcl
-    ls -la
     unzip -q sqlcl-4.2.0.16.355.0402-no-jre.zip
     rm -f sqlcl-4.2.0.16.355.0402-no-jre.zip
     bash mv sqlcl $HOME/.cache/
