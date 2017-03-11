f30dc44839d97b316fff94824e14ed44acb7e98f
Vinicius Avellar <amvinicius@gmail.com>
2017-03-11 09:26:16 -0300

    Calling sh before download.sh.

--- .travis/download_sqlcl.sh
+++ .travis/download_sqlcl.sh
@@ -3,7 +3,7 @@ set -e
 
 cd .travis
 if [ ! -d $HOME/.cache/sqlcl ]; then
-    download.sh -p sqlcl
+    sh download.sh -p sqlcl
     unzip -q $SQLCL_FILE
     bash mv sqlcl $HOME/.cache
     rm -f $SQLCL_FILE
