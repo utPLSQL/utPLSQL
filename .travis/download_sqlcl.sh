4cb84d5b5b50d04aad49e940bfce8f0c28854a60
Vinicius Avellar <amvinicius@gmail.com>
2017-03-11 08:59:28 -0300

    Fix sqlcl download script error.

--- .travis/download_sqlcl.sh
+++ .travis/download_sqlcl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ ! $HOME/.cache/$SQLCL_FILE ]; then
+if [ ! -d $HOME/.cache/sqlcl ]; then
     .travis/download.sh -p sqlcl
     unzip -q $SQLCL_FILE
     bash mv sqlcl $HOME/.cache
