7cd9cd12ac7b3e9f1d338d983ec89068963711a3
Vinicius Avellar Moreira <amvinicius@gmail.com>
2017-03-13 09:47:09 -0300

    Changing password when running the container.

--- .travis/start_db.sh
+++ .travis/start_db.sh
@@ -19,6 +19,7 @@ if [ $ORACLE_VERSION = $ORACLE_12cR1SE ]; then
     docker login -u "$DOCKER_12cR1SE_USER" -p "$DOCKER_12cR1SE_PASS"
     docker run -d --name $ORACLE_VERSION -p 1521:1521 -v $HOME/$ORACLE_VERSION:/opt/oracle/oradata viniciusam/oracle-12c-r1-se
     docker logs -f $ORACLE_VERSION | grep -m 1 "DATABASE IS READY TO USE!" --line-buffered
+    docker exec $ORACLE_VERSION ./setPassword.sh $ORACLE_PWD
 fi
 
 # Oracle 11g R2 XE
@@ -26,12 +27,11 @@ if [ $ORACLE_VERSION = $ORACLE_11gR2XE ]; then
     docker login -u "$DOCKER_11gR2XE_USER" -p "$DOCKER_11gR2XE_PASS"
     docker run -d --name $ORACLE_VERSION --shm-size=1g -p 1521:1521 -v $HOME/$ORACLE_VERSION:/u01/app/oracle/oradata vavellar/oracle-11g-r2-xe
     docker logs -f $ORACLE_VERSION | grep -m 1 "DATABASE IS READY TO USE!" --line-buffered
+    docker exec $ORACLE_VERSION ./setPassword.sh $ORACLE_PWD
 fi
 
 # Save the oracledata dir to cache.
 if [ ! -f $CACHE_DIR/$ORACLE_VERSION.tar.gz ]; then
-    echo "Changing oracle password to: $ORACLE_PWD"
-    docker exec $ORACLE_VERSION ./setPassword.sh $ORACLE_PWD
     echo "Saving database to cache..."
     docker stop $ORACLE_VERSION
     sudo tar -zcf $CACHE_DIR/$ORACLE_VERSION.tar.gz $HOME/$ORACLE_VERSION
