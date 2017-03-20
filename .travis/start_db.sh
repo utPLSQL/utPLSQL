4b8d5f81a6a0c05371c8c82118e48629aad71e52
Vinicius Avellar Moreira <amvinicius@gmail.com>
2017-03-20 12:59:55 -0300

    Fix .docker cache creation.

--- .travis/start_db.sh
+++ .travis/start_db.sh
@@ -6,9 +6,9 @@ DOCKER_BASE_TAG="viniciusam/oracledb"
 # Private Repo Login
 if [ ! -f $CACHE_DIR/config.json ]; then
     docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
-    cp $HOME/.docker/config.json $CACHE_DIR/.docker/config.json
+    mkdir -p $CACHE_DIR/.docker && cp $HOME/.docker/config.json $CACHE_DIR/.docker/
 else
-    cp $CACHE_DIR/.docker/config.json $HOME/.docker/config.json
+    mkdir -p $HOME/.docker && cp $CACHE_DIR/.docker/config.json $HOME/.docker/
 fi
 
 # Oracle 12c R1 SE
