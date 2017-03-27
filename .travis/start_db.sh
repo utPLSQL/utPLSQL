7cceac355d85649370affe35d7c258a3d3ed9e82
Pazus <pazzzus@gmail.com>
2017-03-27 22:16:24 +0300

    switched docker image

--- .travis/start_db.sh
+++ .travis/start_db.sh
@@ -12,18 +12,13 @@ else
     mkdir -p $HOME/.docker && cp $CACHE_DIR/.docker/config.json $HOME/.docker/
 fi
 
-docker pull $DOCKER_BASE_TAG:$ORACLE_VERSION
-docker run -d --name $ORACLE_VERSION $DOCKER_OPTIONS -p 1521:1521 $DOCKER_BASE_TAG:$ORACLE_VERSION
-docker logs -f $ORACLE_VERSION | grep -m 1 "DATABASE IS READY TO USE!" --line-buffered
 
 if [ "$ORACLE_VERSION" == "12c-se2-r2" ]; then
-    "$SQLCLI" sys/oracle@//127.0.0.1:1521/ORCLCDB as sysdba <<EOF
-CREATE PLUGGABLE DATABASE ORCLPDB1 ADMIN USER PDBADMIN IDENTIFIED BY "$ORACLE_PWD"
-  FILE_NAME_CONVERT=('/opt/oracle/oradata/ORCLCDB/pdbseed/','/opt/oracle/oradata/ORCLPDB1/');
-ALTER PLUGGABLE DATABASE ORCLPDB1 SAVE STATE;  
-ALTER PLUGGABLE DATABASE ORCLPDB1 OPEN READ WRITE;
-ALTER SESSION SET CONTAINER = ORCLPDB1;
-CREATE TABLESPACE users DATAFILE '/opt/oracle/oradata/ORCLPDB1/users01.dbf' SIZE 1M AUTOEXTEND ON NEXT 1M;
-exit;
-EOF
+mkdir -p /dev/pdbs
+docker pull $DOCKER_BASE_TAG:$ORACLE_VERSION
+docker run -d --name $ORACLE_VERSION $DOCKER_OPTIONS -p 1521:1521 -v /dev/pdbs:/opt/oracle/oradata/pdbs $DOCKER_BASE_TAG:$ORACLE_VERSION
+else
+docker pull $DOCKER_BASE_TAG:$ORACLE_VERSION
+docker run -d --name $ORACLE_VERSION $DOCKER_OPTIONS -p 1521:1521 $DOCKER_BASE_TAG:$ORACLE_VERSION
 fi
+docker logs -f $ORACLE_VERSION | grep -m 1 "DATABASE IS READY TO USE!" --line-buffered
