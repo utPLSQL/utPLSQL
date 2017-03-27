7408dd7ecad106671fc04beaf589536a04d065a5
Jacek Gębal <jgebal@gmail.com>
2017-03-27 01:58:54 +0100

    Removed unused files. Updated project version to match the last released.

--- .travis/create_release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Change working directory to script directory
-cd "${0%/*}"
-chmod +x ./build_docs.sh
-chmod +x ./create_release_archive.sh
-
-bash ./build_docs.sh
-bash ./create_release_archive.sh
