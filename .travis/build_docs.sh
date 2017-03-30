066ae79223e070a4adb98d0e1fb12ce8c2692188
Jacek Gębal <jgebal@gmail.com>
2017-03-30 20:08:32 +0100

    Fixed missing docs.

--- .travis/build_docs.sh
+++ .travis/build_docs.sh
@@ -4,3 +4,8 @@ cd "${0%/*}"
 # Change back to root
 cd ..
 mkdocs build --clean --strict
+
+mkdir docs/markdown
+mv -t docs/markdown/ docs/about docs/images docs/userguide docs/index.md
+mkdir docs/html
+cp -r -v site/* docs/html
