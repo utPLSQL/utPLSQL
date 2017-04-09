32c7061b226fbd6925027a4712523c394d040e27
Jacek Gębal <jgebal@gmail.com>
2017-04-09 19:31:29 +0100

    Fixed privileges issues needed for coverage (added grant select any dictionary). Fixed issue with weak password for ut3 user (some databases have password policies that require stronger passwords). Added grant on DBMS_LOB, some user reported that this was not granted to public on his database. Updated documentation to reflect changes.

--- source/install_headless.sql
+++ source/install_headless.sql
@@ -15,7 +15,7 @@
   limitations under the License.
 */
 define ut3_user       = ut3
-define ut3_password   = ut3
+define ut3_password   = zyP8KwFfm2XJJSkz
 define ut3_tablespace = users
 
 @@create_utplsql_owner.sql &&ut3_user &&ut3_password &&ut3_tablespace
