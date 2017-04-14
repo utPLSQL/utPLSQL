af2a66af70415f617bc472eb7f9224a2c7590c8b
Jacek Gębal <jgebal@gmail.com>
2017-04-14 23:47:40 +0100

    Moved away from dba_source to all_source.

--- tests/ut_suite_manager/ut_suite_manager.CacheInvalidaesOnPackageDrop.sql
+++ tests/ut_suite_manager/ut_suite_manager.CacheInvalidaesOnPackageDrop.sql
@@ -1,12 +1,12 @@
 set termout off
-create or replace package tst$package_to_be_dropped as
+create or replace package tst_package_to_be_dropped as
   --%suite
-  
+
   --%test
   procedure test1;
 end;
 /
-create or replace package body tst$package_to_be_dropped as
+create or replace package body tst_package_to_be_dropped as
   procedure test1 is begin ut.expect(1).to_equal(1); end;
   procedure test2 is begin ut.expect(1).to_equal(1); end;
 end;
@@ -17,30 +17,30 @@ declare
   l_objects_to_run ut_suite_items;
   test_result number;
 begin
-  ut.run(USER||'.tst$package_to_be_dropped');
+  ut.run(USER||'.tst_package_to_be_dropped');
 end;
 /
-  
-drop package tst$package_to_be_dropped
+
+drop package tst_package_to_be_dropped
 /
 begin
 
   begin
-    ut.run(user || '.tst$package_to_be_dropped');
+    ut.run(user || '.tst_package_to_be_dropped');
   exception
     when others then
-      if sqlerrm like '%tst$package_to_be_dropped%does not exist%' then
+      if sqlerrm like '%tst_package_to_be_dropped%does not exist%' then
         :test_result := ut_utils.tr_success;
       end if;
   end;
 
   if :test_result != ut_utils.tr_success or :test_result is null then
-    dbms_output.put_line('Failed: Expected exception with text like ''%tst$package_to_be_dropped%does not exist%'' but got:''' ||
+    dbms_output.put_line('Failed: Expected exception with text like ''%tst_package_to_be_dropped%does not exist%'' but got:''' ||
                          sqlerrm || '''');
   end if;
 end;
 /
 set termout off
-drop package tst$package_to_be_dropped
+drop package tst_package_to_be_dropped
 /
 set termout on
