8a68b7dc0be9034297d305f0844ce8d15eebd0a1
Jacek Gębal <jgebal@gmail.com>
2017-04-19 00:27:03 +0100

    Fixed Unit Tests after merging.

--- tests/ut_expectations/ut.expect.to_equal.cursor.SupportsSQLandPLSQLdatatypes.sql
+++ tests/ut_expectations/ut.expect.to_equal.cursor.SupportsSQLandPLSQLdatatypes.sql
@@ -1,24 +1,3 @@
-create or replace type utplsql_test_reporter under ut_reporter_base(
-  constructor function utplsql_test_reporter(self in out nocopy utplsql_test_reporter) return self as result,
-  overriding member procedure after_calling_run(self in out nocopy utplsql_test_reporter, a_run in ut_run)
-)
-/
-
-create or replace type body utplsql_test_reporter is
-  constructor function utplsql_test_reporter(self in out nocopy utplsql_test_reporter) return self as result is
-  begin
-    self.init($$plsql_unit);
-    return;
-  end;
-
-  overriding member procedure after_calling_run(self in out nocopy utplsql_test_reporter, a_run in ut_run) is
-  begin
-    self.print_text(a_run.result);
-  end;
-end;
-/
-
-
 create or replace package ut_equal_sys_refcursor_tests is
   --%suite(ut_equal on sys_refcursor data)
   --%suitepath(org.utplsql.test.expectations.equal.refcursor)
@@ -117,7 +96,6 @@ end;
 /
 
 drop package ut_equal_sys_refcursor_tests;
-drop type utplsql_test_reporter;
 
 --workaround to invalidate cache
 exec ut_suite_manager.invalidate_cache();
