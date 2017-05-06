1b9e81a3f7804e50ed4b8b107e88a085019bdb41
Pazus <pazzzus@gmail.com>
2017-05-06 10:44:56 +0300

    fix

--- tests/ut_expectations/common/ut.expect.not_to_be_between.scalar.common.sql
+++ tests/ut_expectations/common/ut.expect.not_to_be_between.scalar.common.sql
@@ -1,12 +1,12 @@
 --Arrange
 declare
   l_actual   &&1 := &&2;
-  l_expected_2 &&1 := &&3;
-  l_expected_1 &&1 := &&4;
+  l_expected_1 &&1 := &&3;
+  l_expected_2 &&1 := &&4;
   l_result   integer;
 begin
 --Act
-  ut.expect(l_actual).to_be_between(l_expected_1,l_expected_2);
+  ut.expect(l_actual).not_to_be_between(l_expected_1,l_expected_2);
   l_result :=  ut_expectation_processor.get_status();
 --Assert
   if nvl(:test_result, ut_utils.tr_success) = ut_utils.tr_success and l_result = &&5 then
--- tests/ut_expectations/common/ut.expect.not_to_equal.scalar.common.sql
+++ tests/ut_expectations/common/ut.expect.not_to_equal.scalar.common.sql
@@ -5,7 +5,7 @@ declare
   l_result   integer;
 begin
 --Act
-  ut.expect(l_actual).to_equal(l_expected);
+  ut.expect(l_actual).not_to_equal(l_expected);
   l_result :=  ut_expectation_processor.get_status();
 --Assert
   if nvl(:test_result, ut_utils.tr_success) = ut_utils.tr_success and l_result = &&4 then
