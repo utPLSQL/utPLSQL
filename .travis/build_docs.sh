9d8ae5c4429bd705a3d6c21dcee82a1c047b4f44
Jacek <jgebal@users.noreply.github.com>
2017-05-14 23:45:33 +0100

    Updated and enabled script to publish project documentation on each build from a tag or develop branch

--- .travis/build_docs.sh
+++ .travis/build_docs.sh
@@ -4,8 +4,3 @@ cd "${0%/*}"
 # Change back to root
 cd ..
 mkdocs build --clean --strict
-
-mkdir docs/markdown
-mv -t docs/markdown/ docs/about docs/images docs/userguide docs/index.md
-mkdir docs/html
-cp -r -v site/* docs/html
