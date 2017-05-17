f04c0732b1baf6521147b488a2eac5b5b1963c79
Jacek <jgebal@users.noreply.github.com>
2017-05-17 06:57:49 +0100

    Fixes to install process
    - Updated to install scripts to fail on bad schema_name
    - Updated readme files to make it more explicit that SYSDBA access is needed for install

--- source/create_synonyms_and_grants_for_user.sql
+++ source/create_synonyms_and_grants_for_user.sql
@@ -22,86 +22,90 @@ set feedback on
 set heading off
 set verify off
 
+prompt Granting privileges on UTPLSQL objects in &&ut3_owner schema to user &&ut3_user
+
 define ut3_owner       = &1
 define ut3_user        = &2
 
-prompt Granting privileges on UTPLSQL objects in &&ut3_owner schema to user &&ut3_user
+whenever sqlerror exit failure rollback
+whenever oserror exit failure rollback
+
 alter session set current_schema = &&ut3_owner;
 
-grant execute on ut_be_between to &ut3_user;
-grant execute on ut_be_empty to &ut3_user;
-grant execute on ut_be_false to &ut3_user;
-grant execute on ut_be_greater_or_equal to &ut3_user;
-grant execute on ut_be_greater_than to &ut3_user;
-grant execute on ut_be_less_or_equal to &ut3_user;
-grant execute on ut_be_less_than to &ut3_user;
-grant execute on ut_be_like to &ut3_user;
-grant execute on ut_be_not_null to &ut3_user;
-grant execute on ut_be_null to &ut3_user;
-grant execute on ut_be_true to &ut3_user;
-grant execute on ut_equal to &ut3_user;
-grant execute on ut_match to &ut3_user;
-grant execute on ut to &ut3_user;
-grant execute on ut_runner to &ut3_user;
-grant execute on ut_teamcity_reporter to &ut3_user;
-grant execute on ut_xunit_reporter to &ut3_user;
-grant execute on ut_documentation_reporter to &ut3_user;
-grant execute on ut_coverage_html_reporter to &ut3_user;
-grant execute on ut_coverage_sonar_reporter to &ut3_user;
-grant execute on ut_coveralls_reporter to &ut3_user;
-grant execute on ut_reporters to &ut3_user;
-grant execute on ut_varchar2_list to &ut3_user;
-grant execute on ut_varchar2_rows to &ut3_user;
-grant execute on ut_reporter_base to &ut3_user;
-grant execute on ut_coverage to &ut3_user;
-grant execute on ut_coverage_options to &ut3_user;
-grant execute on ut_coverage_helper to &ut3_user;
-grant insert, delete, select on ut_coverage_sources_tmp to &ut3_user;
-grant execute on ut_output_buffer to &ut3_user;
-grant execute on ut_file_mappings to &ut3_user;
-grant execute on ut_file_mapping to &ut3_user;
-grant execute on ut_file_mapper to &ut3_user;
-grant execute on ut_key_value_pairs to &ut3_user;
-grant execute on ut_key_value_pair to &ut3_user;
-grant select, insert, delete on ut_cursor_data to &ut3_user;
-grant execute on ut_sonar_test_reporter to &ut3_user;
+grant execute on &&ut3_owner..ut_be_between to &ut3_user;
+grant execute on &&ut3_owner..ut_be_empty to &ut3_user;
+grant execute on &&ut3_owner..ut_be_false to &ut3_user;
+grant execute on &&ut3_owner..ut_be_greater_or_equal to &ut3_user;
+grant execute on &&ut3_owner..ut_be_greater_than to &ut3_user;
+grant execute on &&ut3_owner..ut_be_less_or_equal to &ut3_user;
+grant execute on &&ut3_owner..ut_be_less_than to &ut3_user;
+grant execute on &&ut3_owner..ut_be_like to &ut3_user;
+grant execute on &&ut3_owner..ut_be_not_null to &ut3_user;
+grant execute on &&ut3_owner..ut_be_null to &ut3_user;
+grant execute on &&ut3_owner..ut_be_true to &ut3_user;
+grant execute on &&ut3_owner..ut_equal to &ut3_user;
+grant execute on &&ut3_owner..ut_match to &ut3_user;
+grant execute on &&ut3_owner..ut to &ut3_user;
+grant execute on &&ut3_owner..ut_runner to &ut3_user;
+grant execute on &&ut3_owner..ut_teamcity_reporter to &ut3_user;
+grant execute on &&ut3_owner..ut_xunit_reporter to &ut3_user;
+grant execute on &&ut3_owner..ut_documentation_reporter to &ut3_user;
+grant execute on &&ut3_owner..ut_coverage_html_reporter to &ut3_user;
+grant execute on &&ut3_owner..ut_coverage_sonar_reporter to &ut3_user;
+grant execute on &&ut3_owner..ut_coveralls_reporter to &ut3_user;
+grant execute on &&ut3_owner..ut_reporters to &ut3_user;
+grant execute on &&ut3_owner..ut_varchar2_list to &ut3_user;
+grant execute on &&ut3_owner..ut_varchar2_rows to &ut3_user;
+grant execute on &&ut3_owner..ut_reporter_base to &ut3_user;
+grant execute on &&ut3_owner..ut_coverage to &ut3_user;
+grant execute on &&ut3_owner..ut_coverage_options to &ut3_user;
+grant execute on &&ut3_owner..ut_coverage_helper to &ut3_user;
+grant insert, delete, select on &&ut3_owner..ut_coverage_sources_tmp to &ut3_user;
+grant execute on &&ut3_owner..ut_output_buffer to &ut3_user;
+grant execute on &&ut3_owner..ut_file_mappings to &ut3_user;
+grant execute on &&ut3_owner..ut_file_mapping to &ut3_user;
+grant execute on &&ut3_owner..ut_file_mapper to &ut3_user;
+grant execute on &&ut3_owner..ut_key_value_pairs to &ut3_user;
+grant execute on &&ut3_owner..ut_key_value_pair to &ut3_user;
+grant select, insert, delete on &&ut3_owner..ut_cursor_data to &ut3_user;
+grant execute on &&ut3_owner..ut_sonar_test_reporter to &ut3_user;
 
 prompt Creating synonyms for UTPLSQL objects in &&ut3_owner schema to user &&ut3_user
 
-create or replace synonym &ut3_user .be_between for be_between;
-create or replace synonym &ut3_user .be_empty for be_empty;
-create or replace synonym &ut3_user .be_false for be_false;
-create or replace synonym &ut3_user .be_greater_or_equal for be_greater_or_equal;
-create or replace synonym &ut3_user .be_greater_than for be_greater_than;
-create or replace synonym &ut3_user .be_less_or_equal for be_less_or_equal;
-create or replace synonym &ut3_user .be_less_than for be_less_than;
-create or replace synonym &ut3_user .be_like for be_like;
-create or replace synonym &ut3_user .be_not_null for be_not_null;
-create or replace synonym &ut3_user .be_null for be_null;
-create or replace synonym &ut3_user .be_true for be_true;
-create or replace synonym &ut3_user .equal for equal;
-create or replace synonym &ut3_user .match for match;
-create or replace synonym &ut3_user .ut for ut;
-create or replace synonym &ut3_user .ut_runner for ut_runner;
-create or replace synonym &ut3_user .ut_teamcity_reporter for ut_teamcity_reporter;
-create or replace synonym &ut3_user .ut_xunit_reporter for ut_xunit_reporter;
-create or replace synonym &ut3_user .ut_documentation_reporter for ut_documentation_reporter;
-create or replace synonym &ut3_user .ut_coverage_html_reporter for ut_coverage_html_reporter;
-create or replace synonym &ut3_user .ut_coverage_sonar_reporter for ut_coverage_sonar_reporter;
-create or replace synonym &ut3_user .ut_coveralls_reporter for ut_coveralls_reporter;
-create or replace synonym &ut3_user .ut_reporters for ut_reporters;
-create or replace synonym &ut3_user .ut_varchar2_list for ut_varchar2_list;
-create or replace synonym &ut3_user .ut_varchar2_rows for ut_varchar2_rows;
-create or replace synonym &ut3_user .ut_reporter_base for ut_reporter_base;
-create or replace synonym &ut3_user .ut_coverage for ut_coverage;
-create or replace synonym &ut3_user .ut_coverage_options for ut_coverage_options;
-create or replace synonym &ut3_user .ut_coverage_helper for ut_coverage_helper;
-create or replace synonym &ut3_user .ut_coverage_sources_tmp for ut_coverage_sources_tmp;
-create or replace synonym &ut3_user .ut_output_buffer for ut_output_buffer;
-create or replace synonym &ut3_user .ut_file_mappings for ut_file_mappings;
-create or replace synonym &ut3_user .ut_file_mapping for ut_file_mapping;
-create or replace synonym &ut3_user .ut_file_mapper for ut_file_mapper;
-create or replace synonym &ut3_user .ut_key_value_pairs for ut_key_value_pairs;
-create or replace synonym &ut3_user .ut_key_value_pair for ut_key_value_pair;
-create or replace synonym &ut3_user .ut_cursor_data for ut_cursor_data;
-create or replace synonym &ut3_user .ut_sonar_test_reporter for ut_sonar_test_reporter;
+create or replace synonym &ut3_user..be_between for &&ut3_owner..be_between;
+create or replace synonym &ut3_user..be_empty for &&ut3_owner..be_empty;
+create or replace synonym &ut3_user..be_false for &&ut3_owner..be_false;
+create or replace synonym &ut3_user..be_greater_or_equal for &&ut3_owner..be_greater_or_equal;
+create or replace synonym &ut3_user..be_greater_than for &&ut3_owner..be_greater_than;
+create or replace synonym &ut3_user..be_less_or_equal for &&ut3_owner..be_less_or_equal;
+create or replace synonym &ut3_user..be_less_than for &&ut3_owner..be_less_than;
+create or replace synonym &ut3_user..be_like for &&ut3_owner..be_like;
+create or replace synonym &ut3_user..be_not_null for &&ut3_owner..be_not_null;
+create or replace synonym &ut3_user..be_null for &&ut3_owner..be_null;
+create or replace synonym &ut3_user..be_true for &&ut3_owner..be_true;
+create or replace synonym &ut3_user..equal for &&ut3_owner..equal;
+create or replace synonym &ut3_user..match for &&ut3_owner..match;
+create or replace synonym &ut3_user..ut for &&ut3_owner..ut;
+create or replace synonym &ut3_user..ut_runner for &&ut3_owner..ut_runner;
+create or replace synonym &ut3_user..ut_teamcity_reporter for &&ut3_owner..ut_teamcity_reporter;
+create or replace synonym &ut3_user..ut_xunit_reporter for &&ut3_owner..ut_xunit_reporter;
+create or replace synonym &ut3_user..ut_documentation_reporter for &&ut3_owner..ut_documentation_reporter;
+create or replace synonym &ut3_user..ut_coverage_html_reporter for &&ut3_owner..ut_coverage_html_reporter;
+create or replace synonym &ut3_user..ut_coverage_sonar_reporter for &&ut3_owner..ut_coverage_sonar_reporter;
+create or replace synonym &ut3_user..ut_coveralls_reporter for &&ut3_owner..ut_coveralls_reporter;
+create or replace synonym &ut3_user..ut_reporters for &&ut3_owner..ut_reporters;
+create or replace synonym &ut3_user..ut_varchar2_list for &&ut3_owner..ut_varchar2_list;
+create or replace synonym &ut3_user..ut_varchar2_rows for &&ut3_owner..ut_varchar2_rows;
+create or replace synonym &ut3_user..ut_reporter_base for &&ut3_owner..ut_reporter_base;
+create or replace synonym &ut3_user..ut_coverage for &&ut3_owner..ut_coverage;
+create or replace synonym &ut3_user..ut_coverage_options for &&ut3_owner..ut_coverage_options;
+create or replace synonym &ut3_user..ut_coverage_helper for &&ut3_owner..ut_coverage_helper;
+create or replace synonym &ut3_user..ut_coverage_sources_tmp for &&ut3_owner..ut_coverage_sources_tmp;
+create or replace synonym &ut3_user..ut_output_buffer for &&ut3_owner..ut_output_buffer;
+create or replace synonym &ut3_user..ut_file_mappings for &&ut3_owner..ut_file_mappings;
+create or replace synonym &ut3_user..ut_file_mapping for &&ut3_owner..ut_file_mapping;
+create or replace synonym &ut3_user..ut_file_mapper for &&ut3_owner..ut_file_mapper;
+create or replace synonym &ut3_user..ut_key_value_pairs for &&ut3_owner..ut_key_value_pairs;
+create or replace synonym &ut3_user..ut_key_value_pair for &&ut3_owner..ut_key_value_pair;
+create or replace synonym &ut3_user..ut_cursor_data for &&ut3_owner..ut_cursor_data;
+create or replace synonym &ut3_user..ut_sonar_test_reporter for &&ut3_owner..ut_sonar_test_reporter;
