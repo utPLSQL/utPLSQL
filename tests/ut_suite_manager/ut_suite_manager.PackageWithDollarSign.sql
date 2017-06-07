cf49d757081084da4a8346519fdc8e56f3733df7
Pazus <pazzzus@gmail.com>
2017-06-07 23:28:01 +0300

    fixing $ and # sign in user and procedures fixed dollar escape in skip_by_path method changed ut3_user to ut3$user# (escaped variable in travis)

---
+++ tests/ut_suite_manager/ut_suite_manager.PackageWithDollarSign.sql
@@ -0,0 +1,31 @@
+set termout off
+create or replace package tst_package_with$dollar as
+  --%suite
+
+  --%test
+  procedure test1;
+end;
+/
+
+create or replace package body tst_package_with$dollar as
+  procedure test1 is begin ut.expect(1).to_equal(1); end;
+  procedure test2 is begin ut.expect(1).to_equal(1); end;
+end;
+/
+
+set termout on
+
+declare
+  l_objects_to_run ut_suite_items;
+begin
+  --act
+  l_objects_to_run := ut_suite_manager.configure_execution_by_path(ut_varchar2_list('tst_package_with$dollar'));
+ 
+  if ut_expectation_processor.get_status = ut_utils.tr_success then
+    :test_result := ut_utils.tr_success;
+  end if;
+end;
+/
+
+drop package tst_package_with$dollar
+/
