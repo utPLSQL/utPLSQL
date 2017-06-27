fba1291c2d00f8737623673afa5a79b24681b9c3
Jacek <jgebal@users.noreply.github.com>
2017-06-27 21:32:13 +0100

    Updated install/uninstall scripts to improve readability.

--- source/install_component.sql
+++ source/install_component.sql
@@ -1,8 +1,6 @@
 set heading off
 set feedback off
 exec dbms_output.put_line('Installing component '||upper(regexp_substr('&&1','\/(\w*)\.',1,1,'i',1)));
-set feedback on
 @@&&1
-set feedback off
 exec dbms_output.put_line('&&line_separator');
 
