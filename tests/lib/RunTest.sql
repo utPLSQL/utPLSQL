51a0092b569edf65dd89b90d065a136374c05a11
Jacek <jgebal@users.noreply.github.com>
2017-07-03 08:53:57 +0100

    Improved reporting from RunTest

--- tests/lib/RunTest.sql
+++ tests/lib/RunTest.sql
@@ -4,7 +4,8 @@ exec :test_result := null;
 prompt Executing test: &1
 exec :test_start_time := dbms_utility.get_time;
 @@&1
-
+set serveroutput on size unlimited format truncated
+set termout on
 declare
   l_duration_str varchar2(300) := ', executed in: '||((dbms_utility.get_time - :test_start_time)/100)||' second(s)';
 begin
@@ -12,7 +13,9 @@ begin
      when :test_result = ut_utils.tr_success then
         :successes_count := :successes_count + 1;
      else
+       dbms_output.put_line('---------------------------------------');
        dbms_output.put_line('  Failure'||l_duration_str);
+       dbms_output.put_line('---------------------------------------');
        :failures_count := :failures_count + 1;
   end case;
 end;
