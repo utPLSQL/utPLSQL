169c7c2133d919399dab2a81bd92a60d0244e74e
Jacek <jgebal@users.noreply.github.com>
2017-07-22 00:46:29 +0100

    Adding additional installation of utPLSQL framework into schema ut3_release

---
+++ .travis/install_utplsql_release.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+RELEASE_VERSION_SCHEMA=UT3_LATEST_RELEASE
+set -ev
+
+cd $UTPLSQL_DIR/source
+
+"$SQLCLI" sys/$ORACLE_PWD@//$CONNECTION_STR AS SYSDBA <<-SQL
+declare
+  i integer := 0;
+begin
+  dbms_output.put_line('Dropping synonyms pointing to schema '||upper('${UT3_OWNER}'));
+  for syn in (
+    select
+      case when owner = 'PUBLIC'
+        then 'public synonym '
+        else 'synonym ' || owner || '.' end || synonym_name as syn_name,
+      table_owner||'.'||table_name as for_object
+    from all_synonyms s
+    where table_owner = upper('${UT3_OWNER}') and table_owner != owner
+  )
+  loop
+    i := i + 1;
+    begin
+      execute immediate 'drop '||syn.syn_name;
+      dbms_output.put_line('Dropped '||syn.syn_name||' for object '||syn.for_object);
+    exception
+      when others then
+        dbms_output.put_line('FAILED to drop '||syn.syn_name||' for object '||syn.for_object);
+    end;
+  end loop;
+  dbms_output.put_line('&&line_separator');
+  dbms_output.put_line(i||' synonyms dropped');
+end;
+/
+SQL
+"$SQLCLI" sys/$ORACLE_PWD@//$CONNECTION_STR AS SYSDBA @install_headless.sql ${RELEASE_VERSION_SCHEMA}
+
+"$SQLCLI" sys/$ORACLE_PWD@//$CONNECTION_STR AS SYSDBA <<SQL
+grant select any dictionary to ${RELEASE_VERSION_SCHEMA};
+exit
+SQL
