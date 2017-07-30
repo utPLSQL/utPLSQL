b7c59176b6c1446b5a2b2c0763724baf1b0f12da
Pazus <pazzzus@gmail.com>
2017-07-30 12:18:39 +0300

    implemented matcher tests

--- test/install_tests.sql
+++ test/install_tests.sql
@@ -1,9 +1,11 @@
 
 @ut_utils/test_ut_utils.pks
 @ut_annotations/test_annotations.pks
+@ut_matchers/test_matchers.pks
 
 @ut_utils/test_ut_utils.pkb
 @ut_annotations/test_annotations.pkb
+@ut_matchers/test_matchers.pkb
 
 show errors
 
