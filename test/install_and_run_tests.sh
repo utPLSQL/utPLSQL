d34c9a69c5010ecbaaf9ac1532fcdb23b188855d
Jacek <jgebal@users.noreply.github.com>
2017-08-03 00:55:55 +0100

    Updated test script to capture failed tests and return non-zero exit code. Fixed sonar properties. Moved new tests to the end of the build process.

--- test/install_and_run_tests.sh
+++ test/install_and_run_tests.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 set -ev
 
-cd test
+#cd test
+#
+#"$SQLCLI" ${UT3_TESTER}/${UT3_TESTER_PASSWORD}@//${CONNECTION_STR} @install_tests.sql
+#
+#cd ..
+#
+#utPLSQL-cli/bin/utplsql run ${UT3_TESTER}/${UT3_TESTER_PASSWORD}@${CONNECTION_STR} \
+#-source_path=source -test_path=tests \
+#-f=ut_documentation_reporter  -c -o=test_results.log -s \
+#-f=ut_coverage_sonar_reporter -o=coverage.xml \
+#-f=ut_sonar_test_reporter     -o=test_results.xml
 
-"$SQLCLI" ${UT3_TESTER}/${UT3_TESTER_PASSWORD}@//${CONNECTION_STR} @install_tests.sql
+status_line_regex="^[0-9]+ tests, ([0-9]+) failed, ([0-9]+) errored.*"
 
-cd ..
+RC=$(cat test_results.log | grep -E "${status_line_regex}" | sed -re "s/${status_line_regex}/\1\2/")
 
-utPLSQL-cli/bin/utplsql run ${UT3_TESTER}/${UT3_TESTER_PASSWORD}@${CONNECTION_STR} \
--source_path=source -test_path=tests \
--f=ut_documentation_reporter  -c \
--f=ut_coverage_sonar_reporter -o=coverage.xml \
--f=ut_sonar_test_reporter     -o=test_results.xml
+exit $RC
