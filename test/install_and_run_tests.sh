1b2dd8c95d0150b4355bbc82d93e6ceba94b882b
Jacek <jgebal@users.noreply.github.com>
2017-08-03 01:20:26 +0100

    Enabling tests.

--- test/install_and_run_tests.sh
+++ test/install_and_run_tests.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -ev
 
-#cd test
-#
-#"$SQLCLI" ${UT3_TESTER}/${UT3_TESTER_PASSWORD}@//${CONNECTION_STR} @install_tests.sql
-#
-#cd ..
-#
-#utPLSQL-cli/bin/utplsql run ${UT3_TESTER}/${UT3_TESTER_PASSWORD}@${CONNECTION_STR} \
-#-source_path=source -test_path=tests \
-#-f=ut_documentation_reporter  -c -o=test_results.log -s \
-#-f=ut_coverage_sonar_reporter -o=coverage.xml \
-#-f=ut_sonar_test_reporter     -o=test_results.xml
+cd test
+
+"$SQLCLI" ${UT3_TESTER}/${UT3_TESTER_PASSWORD}@//${CONNECTION_STR} @install_tests.sql
+
+cd ..
+
+utPLSQL-cli/bin/utplsql run ${UT3_TESTER}/${UT3_TESTER_PASSWORD}@${CONNECTION_STR} \
+-source_path=source -test_path=tests \
+-f=ut_documentation_reporter  -c -o=test_results.log -s \
+-f=ut_coverage_sonar_reporter -o=coverage.xml \
+-f=ut_sonar_test_reporter     -o=test_results.xml
 
 status_line_regex="^[0-9]+ tests, ([0-9]+) failed, ([0-9]+) errored.*"
 
