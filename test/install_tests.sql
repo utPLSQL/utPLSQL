2155b991e58477bc8738c85e292069b4f093d455
Pazus <pazzzus@gmail.com>
2017-08-05 11:10:23 +0300

    added ut_suite_manager tests

--- test/install_tests.sql
+++ test/install_tests.sql
@@ -4,12 +4,14 @@
 @ut_annotations/test_annotations.pks
 @ut_matchers/test_matchers.pks
 @ut_output_buffer/test_output_buffer.pks
+@ut_suite_manager/test_suite_manager.pks
 
 @core.pkb
 @ut_utils/test_ut_utils.pkb
 @ut_annotations/test_annotations.pkb
 @ut_matchers/test_matchers.pkb
 @ut_output_buffer/test_output_buffer.pkb
+@ut_suite_manager/test_suite_manager.pkb
 
 show errors
 
