4121aaf1c97462bb035c55f827a0490053d7a167
Pazus <pazzzus@gmail.com>
2017-08-06 08:51:10 +0300

    changed path to sources

--- test/install_and_run_tests.sh
+++ test/install_and_run_tests.sh
@@ -5,10 +5,10 @@ cd test
 
 "$SQLCLI" ${UT3_TESTER}/${UT3_TESTER_PASSWORD}@//${CONNECTION_STR} @install_tests.sql
 
-cd ..
+cd ../..
 
 utPLSQL-cli/bin/utplsql run ${UT3_TESTER}/${UT3_TESTER_PASSWORD}@${CONNECTION_STR} \
--source_path=../source -test_path=test -c \
+-source_path=source -test_path=test -c \
 -f=ut_documentation_reporter  -o=test_results.log -s \
 -f=ut_coverage_sonar_reporter -o=coverage.xml \
 -f=ut_sonar_test_reporter     -o=test_results.xml
