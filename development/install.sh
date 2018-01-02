35cf28d8859b0deefa778aeb77c1b009844c8760
Jacek Gebal <jgebal@users.noreply.github.com>
2018-01-02 14:40:29 +0000

    Improved error reporting on `development/install.sh` script. The script will now stop on errors and make it easier to see where it failed.

--- development/install.sh
+++ development/install.sh
@@ -5,8 +5,20 @@ git rev-parse && cd "$(git rev-parse --show-cdup)"
 
 . development/env.sh
 
-development/cleanup.sh
-
-.travis/install.sh
-.travis/install_utplsql_release.sh
-.travis/create_additional_grants_for_old_tests.sh
+header="******************************************************************************************"
+if ! development/cleanup.sh; then
+  echo -e ${header}"\nFailed to cleanup utPLSQL environment\n"${header}
+  exit 1
+fi
+if ! .travis/install.sh; then
+  echo -e ${header}"\nFailed to install utPLSQL from current branch into ${UT3_OWNER} schema\n"${header}
+  exit 1
+fi
+if ! .travis/install_utplsql_release.sh; then
+    echo -e ${header}"\nFailed to install utPLSQL from branch ${SELFTESTING_BRANCH} into ${UT3_RELEASE_VERSION_SCHEMA}\n"${header}
+    exit 1
+fi
+if ! .travis/create_additional_grants_for_old_tests.sh; then
+    echo -e ${header}"Failed to add grants needed old_tests\n"${header}
+    exit 1
+fi
