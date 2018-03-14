ac40332d66cf4dd47d8d235f3e8b90e324873030
lwasylow <lwasylow@gmail.com>
2018-03-14 20:43:52 +0000

    Added 12.2 Coverage Amended install script to not fail when block coverage not present

--- source/core/coverage/dbms_plssqlcode.sql
+++ source/core/coverage/dbms_plssqlcode.sql
@@ -1,5 +1,11 @@
+declare
+ e_not_exists exception;
+ pragma exception_init(e_not_exists,-6576);
+ l_install_call varchar2(500) := 'call dbms_plsql_code_coverage.create_coverage_tables(force_it => :forceit)';
 begin
-   DBMS_PLSQL_CODE_COVERAGE.create_coverage_tables(
-    force_it => TRUE);
+ execute immediate l_install_call using in true;
+exception 
+ when e_not_exists then
+  dbms_output.put_line('dbms_plsql_code_coverage doesnt exists in your database. Please upgrade.');
 end;
 /
