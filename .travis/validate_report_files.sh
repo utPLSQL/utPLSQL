36a3f2233a133846cc0fd56e1ebf1532ab842e78
lwasylow <lwasylow@gmail.com>
2018-06-06 23:36:52 +0100

    Added XML Validation

--- .travis/validate_report_files.sh
+++ .travis/validate_report_files.sh
@@ -1,25 +1,72 @@
 #!/usr/bin/env bash
 
-echo "validate html" 
+GL_VALID=1
 
 HTML_FILENAME="coverage.html"
+JUNIT_FILENAME="junit_test_results.xml"
+JUNIT_XSD="xsd\\junit4.xsd"
+TFS_FILENAME="tfs_test_results.xml"
+TFS_XSD="xsd\\junit_windy.xsd"
+
+
 #Exclude existing issue with OL nested in PRE
 EXCLUSION_REGEX=".*Element\s.?ol.?\snot\sallowed\sas\schild\sof\selement\s.?pre.*"
+
+#HTML Validation API
 HTML_VALIDATOR_URL="https://validator.w3.org/nu/"
 VALIDATOR_OUT="gnu"
 WARNING_REGEX="info warning:"
 ERROR_REGEX="error:"
 
-VALIDATION_RESULTS=$(curl -H "Content-Type: text/html; charset=utf-8" --data-binary @$HTML_FILENAME "$HTML_VALIDATOR_URL?out=$VALIDATOR_OUT&filterpattern=$EXCLUSION_REGEX")
+#XML Validator
+XML_VALIDATOR="lib\\xml_validator.jar"
 
-ERROR_COUNT=`echo "$VALIDATION_RESULTS" | grep -c "$ERROR_REGEX"`
-WARNING_COUNT=`echo "$VALIDATION_RESULTS" | grep -c "$WARNING_REGEX"`
+#Validate HTML
+HTML_VALIDATION_RESULTS=$(curl -H "Content-Type: text/html; charset=utf-8" --data-binary @$HTML_FILENAME "$HTML_VALIDATOR_URL?out=$VALIDATOR_OUT&filterpattern=$EXCLUSION_REGEX")
 
-echo "There are $ERROR_COUNT errors, $WARNING_COUNT warning  in $HTML_FILENAME"
+ERROR_COUNT=$(echo "$HTML_VALIDATION_RESULTS" | grep -c "$ERROR_REGEX")
+WARNING_COUNT=$(echo "$HTML_VALIDATION_RESULTS" | grep -c "$WARNING_REGEX")
 
-if [ $ERROR_COUNT -gt 0 ]; then
- echo "$VALIDATION_RESULTS" | grep "$ERROR_REGEX"
- exit 1
+if [ "$ERROR_COUNT" -gt 0 ]; then
+    GL_VALID=0
+    echo ""
+    echo "******************************"
+    echo "HTML File is invalid"
+    echo "There are $ERROR_COUNT errors, $WARNING_COUNT warning  in $HTML_FILENAME"
+    echo "Please see results:"
+    echo "$HTML_VALIDATION_RESULTS" | grep "$ERROR_REGEX"
+fi
+
+
+#Validate XML TFS_FILENAME
+TFS_RESULT=$(java -jar $XML_VALIDATOR -s $TFS_XSD $TFS_FILENAME 2>&1)
+
+if [ $? -ne 0 ]; then
+    GL_VALID=0
+    echo ""
+    echo "******************************"
+    echo "TFS Test Result XML is invalid"
+    echo "Please see results:"
+    echo "$TFS_RESULT"
+fi
+
+#Validate XML JUNIT_FILENAME
+JUNIT_RESULT=$(java -jar $XML_VALIDATOR -s $JUNIT_XSD $JUNIT_FILENAME 2>&1)
+
+if [ $? -ne 0 ]; then
+    GL_VALID=0
+    echo ""
+    echo "******************************"
+    echo "JUNIT Test Result XML is invalid"
+    echo "Please see results:"
+    echo "$JUNIT_RESULT"
+fi
+
+if [ $GL_VALID -ne 1 ]; then
+    echo ""
+    echo "******************************"
+    echo "Validation failed please see results above."
+    exit 1
 else
- exit 0
+    exit 0
 fi
