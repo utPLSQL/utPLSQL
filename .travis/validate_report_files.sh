934e991c1164a2558d89c8dec940de92e3bc7c2f
lwasylow <lwasylow@gmail.com>
2018-06-07 12:51:47 +0100

    Update xml validation file

--- .travis/validate_report_files.sh
+++ .travis/validate_report_files.sh
@@ -1,79 +1,60 @@
 #!/usr/bin/env bash
 
 GL_VALID=1
-
-HTML_FILENAME="coverage.html"
-JUNIT_FILENAME="junit_test_results.xml"
-TFS_FILENAME="tfs_test_results.xml"
 XSD_DIR="$TRAVIS_BUILD_DIR/.travis/xsd"
 XML_JAR_DIR="$TRAVIS_BUILD_DIR/.travis/lib"
-JUNIT_XSD="$XSD_DIR/junit4.xsd"
-TFS_XSD="$XSD_DIR/junit_windy.xsd"
-
-
-#Exclude existing issue with OL nested in PRE
-EXCLUSION_REGEX=".*Element\s.?ol.?\snot\sallowed\sas\schild\sof\selement\s.?pre.*"
-
-#HTML Validation API
-HTML_VALIDATOR_URL="https://validator.w3.org/nu/"
-VALIDATOR_OUT="gnu"
-WARNING_REGEX="info warning:"
-ERROR_REGEX="error:"
-
 #XML Validator
 XML_VALIDATOR="$XML_JAR_DIR/xml_validator.jar"
+HTML_VALIDATOR_URL="https://validator.w3.org/nu/"
 
-#Validate HTML
-HTML_VALIDATION_RESULTS=$(curl -H "Content-Type: text/html; charset=utf-8" --data-binary @$HTML_FILENAME "$HTML_VALIDATOR_URL?out=$VALIDATOR_OUT&filterpattern=$EXCLUSION_REGEX")
+HTML_FILENAME="coverage.html"
+declare -A XML_FILES
+XML_FILES["junit_test_results.xml"]="junit4.xsd"   
+XML_FILES["tfs_test_results.xml"]="junit_windy.xsd"
 
-ERROR_COUNT=$(echo "$HTML_VALIDATION_RESULTS" | grep -c "$ERROR_REGEX")
-WARNING_COUNT=$(echo "$HTML_VALIDATION_RESULTS" | grep -c "$WARNING_REGEX")
+function ValidateHtml {
+    EXCLUSION_REGEX=".*Element\s.?ol.?\snot\sallowed\sas\schild\sof\selement\s.?pre.*"
+    #HTML Validation API
+    VALIDATOR_OUT="gnu"
+    WARNING_REGEX="info warning:"
+    ERROR_REGEX="error:"
+    #Validate HTML
+    HTML_VALIDATION_RESULTS=$(curl -H "Content-Type: text/html; charset=utf-8" --data-binary @$1 "$HTML_VALIDATOR_URL?out=$VALIDATOR_OUT&filterpattern=$EXCLUSION_REGEX")
 
-if [ "$ERROR_COUNT" -gt 0 ]; then
+    ERROR_COUNT=$(echo "$HTML_VALIDATION_RESULTS" | grep -c "$ERROR_REGEX")
+    WARNING_COUNT=$(echo "$HTML_VALIDATION_RESULTS" | grep -c "$WARNING_REGEX")
+    
+    if [ "$ERROR_COUNT" -gt 0 ]; then
     GL_VALID=0
-    echo ""
-    echo "******************************"
-    echo "HTML File is invalid"
-    echo "There are $ERROR_COUNT errors, $WARNING_COUNT warning  in $HTML_FILENAME"
-    echo "Please see results:"
-    echo "$HTML_VALIDATION_RESULTS" | grep "$ERROR_REGEX"
-fi
+        echo ""
+        echo "******************************"
+        echo "HTML File is invalid"
+        echo "There are $ERROR_COUNT errors, $WARNING_COUNT warning  in $HTML_FILENAME"
+        echo "Please see results:"
+        echo "$HTML_VALIDATION_RESULTS" | grep "$ERROR_REGEX"
+    fi
+}
 
+function ValidateXML() {
+    echo "Validate File $1 against schema $2"
+    VALIDATION_RESULT=$(java -jar $XML_VALIDATOR -s $1 $2 2>&1)
+        
+    if [ $? -ne 0 ]; then
+        GL_VALID=0
+        echo ""
+        echo "******************************"
+        echo "XML is invalid"
+        echo "Please see results:"
+        echo "$VALIDATION_RESULT"
+    fi
+}
 
+ValidateHtml "$HTML_FILENAME"
 
-#Debug
-echo "PWD"
-pwd
-
-echo ""
-echo "LS $TRAVIS_BUILD_DIR"]
-ls $TRAVIS_BUILD_DIR
-
-
-
-#Validate XML TFS_FILENAME
-TFS_RESULT=$(java -jar $XML_VALIDATOR -s $TFS_XSD $TFS_FILENAME 2>&1)
-
-if [ $? -ne 0 ]; then
-    GL_VALID=0
-    echo ""
-    echo "******************************"
-    echo "TFS Test Result XML is invalid"
-    echo "Please see results:"
-    echo "$TFS_RESULT"
-fi
-
-#Validate XML JUNIT_FILENAME
-JUNIT_RESULT=$(java -jar $XML_VALIDATOR -s $JUNIT_XSD $JUNIT_FILENAME 2>&1)
-
-if [ $? -ne 0 ]; then
-    GL_VALID=0
-    echo ""
-    echo "******************************"
-    echo "JUNIT Test Result XML is invalid"
-    echo "Please see results:"
-    echo "$JUNIT_RESULT"
-fi
+for XMLFILE in "${!XML_FILES[@]}"; do 
+    #echo "$XMLFILE" "${XML_FILES[$XMLFILE]}"; 
+    ValidateXML "$XMLFILE" "XSD_DIR/${XML_FILES[$XMLFILE]}"
+done
 
 if [ $GL_VALID -ne 1 ]; then
     echo ""
@@ -81,7 +62,5 @@ if [ $GL_VALID -ne 1 ]; then
     echo "Validation failed please see results above."
     exit 1
 else
-    echo ""
-    echo "******************************"
-    echo "Validation passed. Good Job!"
+    exit 0
 fi
