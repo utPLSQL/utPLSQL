110bfa743b06a31353d9157ec9686355420cb8ea
Jacek Gebal <jgebal@users.noreply.github.com>
2018-07-29 16:37:19 +0100

    Disabling maven wagon-http

--- .travis/maven_cfg.sh
+++ .travis/maven_cfg.sh
@@ -2,17 +2,20 @@
 set -ev
 cp .travis/settings.xml $MAVEN_CFG/settings.xml
 
-cd $(dirname $(readlink -f $0))
-
-# Download wagon-http recommended by Oracle.
-# On maven latest version this is not needed, but travis doesn't have it.
-if [ ! -f $CACHE_DIR/wagon-http-2.8-shaded.jar ]; then
-    curl -L -O "http://central.maven.org/maven2/org/apache/maven/wagon/wagon-http/2.8/wagon-http-2.8-shaded.jar"
-    mv wagon-http-2.8-shaded.jar $CACHE_DIR/
-    sudo cp $CACHE_DIR/wagon-http-2.8-shaded.jar $MAVEN_HOME/lib/ext/
-else
-    echo "Using cached wagon-http..."
-    sudo cp $CACHE_DIR/wagon-http-2.8-shaded.jar $MAVEN_HOME/lib/ext/
-fi
-
-mvn dependency:copy-dependencies -DoutputDirectory=../utPLSQL-cli/lib
+############################################
+# disabling all the below to check if it's still needed with maven 3.5.2 on Travis
+############################################
+#cd $(dirname $(readlink -f $0))
+#
+## Download wagon-http recommended by Oracle.
+## On maven latest version this is not needed, but travis doesn't have it.
+#if [ ! -f $CACHE_DIR/wagon-http-2.8-shaded.jar ]; then
+#    curl -L -O "http://central.maven.org/maven2/org/apache/maven/wagon/wagon-http/2.8/wagon-http-2.8-shaded.jar"
+#    mv wagon-http-2.8-shaded.jar $CACHE_DIR/
+#    sudo cp $CACHE_DIR/wagon-http-2.8-shaded.jar $MAVEN_HOME/lib/ext/
+#else
+#    echo "Using cached wagon-http..."
+#    sudo cp $CACHE_DIR/wagon-http-2.8-shaded.jar $MAVEN_HOME/lib/ext/
+#fi
+#
+#mvn dependency:copy-dependencies -DoutputDirectory=../utPLSQL-cli/lib
