94234573bad50053c8e486acd36f8d084f4d5c63
lwasylow <lwasylow@gmail.com>
2018-10-28 07:48:16 +0000

    Added test for min user test

--- test/install_and_run_tests.sh
+++ test/install_and_run_tests.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 set -ev
 
+check_result()
+{
+  RC1=$1
+  RC2=$2
+
+  if [ "$RC1" != "0" ] || [ "$RC2" != "0" ]; then
+   return 1
+  fi
+  return 0
+}
 
 #goto git root directory
 git rev-parse && cd "$(git rev-parse --show-cdup)"
 
+cd test
+
+time "$SQLCLI" ${UT3_USER}/${UT3_USER_PASSWORD}@//${CONNECTION_STR} @install__min_usr_tests.sql
+
+cd ..
+
+time utPLSQL-cli/bin/utplsql run ${UT3_USER}/${UT3_USER_PASSWORD}@${CONNECTION_STR} \
+-source_path=source -owner=ut3 \
+-test_path=test -c \
+-f=ut_documentation_reporter  -o=min_test_results.log -s \
+-scc
+
+status_line_regex="^[0-9]+ tests, ([0-9]+) failed, ([0-9]+) errored.*"
+
+RC1=$(cat min_test_results.log | grep -E "${status_line_regex}" | sed -re "s/${status_line_regex}/\1\2/")
 
 cd test
 
@@ -29,6 +54,9 @@ status_line_regex="^[0-9]+ tests, ([0-9]+) failed, ([0-9]+) errored.*"
 #cat coverage.xml
 #cat test_results.xml
 
-RC=$(cat test_results.log | grep -E "${status_line_regex}" | sed -re "s/${status_line_regex}/\1\2/")
+RC2=$(cat test_results.log | grep -E "${status_line_regex}" | sed -re "s/${status_line_regex}/\1\2/")
+
+check_result $RC1 $RC2
+RC=$?
 
 exit $RC
