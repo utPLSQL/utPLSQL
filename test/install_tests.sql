28a1a73d8e7ddaa3df5f4e8f509a2c7335b74984
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2018-12-28 09:15:05 +0100

    install test_realtime_reporter

--- test/install_tests.sql
+++ test/install_tests.sql
@@ -42,6 +42,7 @@ set define off
 @@core/reporters/test_coverage/test_coveralls_reporter.pks
 @@core/reporters/test_coverage/test_cov_cobertura_reporter.pks
 @@core/reporters/test_junit_reporter.pks
+@@core/reporters/test_realtime_reporter.pks
 set define on
 @@install_below_12_2.sql 'core/reporters/test_coverage/test_html_proftab_reporter.pks'
 set define off
@@ -92,6 +93,7 @@ set define off
 @@core/reporters/test_coverage/test_coveralls_reporter.pkb
 @@core/reporters/test_coverage/test_cov_cobertura_reporter.pkb
 @@core/reporters/test_junit_reporter.pkb
+@@core/reporters/test_realtime_reporter.pkb
 set define on
 @@install_below_12_2.sql 'core/reporters/test_coverage/test_html_proftab_reporter.pkb'
 set define off
