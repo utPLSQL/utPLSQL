4737fde0c9bd4d127b4181296cacb9268e2df30d
Jacek Gebal <jgebal@users.noreply.github.com>
2019-01-12 13:17:22 +0000

    Added select with row limit to assure we dont overload memory when reading data on after-run.

--- test/install_and_run_tests.sh
+++ test/install_and_run_tests.sh
@@ -13,11 +13,11 @@ cd ..
 time utPLSQL-cli/bin/utplsql run ${UT3_TESTER}/${UT3_TESTER_PASSWORD}@${CONNECTION_STR} \
 -source_path=source -owner=ut3 \
 -test_path=test -c \
--f=ut_documentation_reporter  -o=test_results.log -s \
 -f=ut_coverage_sonar_reporter -o=coverage.xml \
 -f=ut_coverage_html_reporter  -o=coverage.html \
 -f=ut_coveralls_reporter      -o=coverage.json \
 -f=ut_sonar_test_reporter     -o=test_results.xml \
 -f=ut_junit_reporter          -o=junit_test_results.xml \
 -f=ut_tfs_junit_reporter      -o=tfs_test_results.xml \
+-f=ut_documentation_reporter  -o=test_results.log -s \
 -scc
