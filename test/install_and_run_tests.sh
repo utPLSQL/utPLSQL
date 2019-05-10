4d7bc3d0a212a8b8520611d29fb6590d37081f5d
Jacek Gebal <jgebal@users.noreply.github.com>
2019-05-11 00:28:42 +0100

    Fixed issue with missing newlines in cobertura coverage for non-executed packages. Resolves #917

--- test/install_and_run_tests.sh
+++ test/install_and_run_tests.sh
@@ -20,10 +20,11 @@ time utPLSQL-cli/bin/utplsql run ${UT3_TESTER_HELPER}/${UT3_TESTER_HELPER_PASSWO
 -source_path=source -owner=ut3 \
 -p='ut3_tester,ut3$user#' \
 -test_path=test -c \
--f=ut_coverage_sonar_reporter -o=coverage.xml \
--f=ut_coverage_html_reporter  -o=coverage.html \
--f=ut_coveralls_reporter      -o=coverage.json \
--f=ut_sonar_test_reporter     -o=test_results.xml \
--f=ut_junit_reporter          -o=junit_test_results.xml \
--f=ut_tfs_junit_reporter      -o=tfs_test_results.xml \
--f=ut_documentation_reporter  -o=test_results.log -s
+-f=ut_coverage_sonar_reporter     -o=coverage.xml \
+-f=ut_coverage_cobertura_reporter -o=cobertura.xml \
+-f=ut_coverage_html_reporter      -o=coverage.html \
+-f=ut_coveralls_reporter          -o=coverage.json \
+-f=ut_sonar_test_reporter         -o=test_results.xml \
+-f=ut_junit_reporter              -o=junit_test_results.xml \
+-f=ut_tfs_junit_reporter          -o=tfs_test_results.xml \
+-f=ut_documentation_reporter      -o=test_results.log -s
