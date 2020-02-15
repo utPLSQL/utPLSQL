8eac19e41a7b20d9b443901cae90cd3429310835
Jacek Gebal <jgebal@users.noreply.github.com>
2020-02-15 15:03:34 +0000

    Changed maven repo url.

--- .travis/maven_cfg.sh
+++ .travis/maven_cfg.sh
@@ -7,7 +7,7 @@ cd $(dirname $(readlink -f $0))
 # Download wagon-http recommended by Oracle.
 # On maven latest version this is not needed, but travis doesn't have it.
 if [ ! -f $CACHE_DIR/wagon-http-2.8-shaded.jar ]; then
-    curl -L -O "http://central.maven.org/maven2/org/apache/maven/wagon/wagon-http/2.8/wagon-http-2.8-shaded.jar"
+    curl -L -O "https://repo1.maven.org/maven2/org/apache/maven/wagon/wagon-http/2.8/wagon-http-2.8-shaded.jar"
     mv wagon-http-2.8-shaded.jar $CACHE_DIR/
     sudo cp $CACHE_DIR/wagon-http-2.8-shaded.jar $MAVEN_HOME/lib/ext/
 else
