179586aa2cf4cd4658efc51400bfc2a7a8032790
Jacek Gebal <jgebal@users.noreply.github.com>
2020-03-15 22:51:20 +0000

    Enabled echo and feedback for examples

--- .travis/run_examples.sh
+++ .travis/run_examples.sh
@@ -5,10 +5,6 @@ cd "$(dirname "$(readlink -f "$0")")"/../examples
 set -ev
 
 "$SQLCLI" $UT3_DEVELOP_SCHEMA/$UT3_DEVELOP_SCHEMA_PASSWORD@//$CONNECTION_STR <<SQL
-set echo on
-set feedback on
-whenever sqlerror exit failure rollback
-whenever oserror exit failure rollback
 
 @RunAllExamplesAsTests.sql
 
