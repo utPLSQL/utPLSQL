0d9eddc4debaa12154f259eeb76e165399d7f8b9
Jacek Gebal <jgebal@users.noreply.github.com>
2020-03-15 22:43:56 +0000

    Fixing examples run

--- .travis/run_examples.sh
+++ .travis/run_examples.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+cd "$(dirname "$(readlink -f "$0")")"/../examples
+
 set -ev
 
 "$SQLCLI" $UT3_DEVELOP_SCHEMA/$UT3_DEVELOP_SCHEMA_PASSWORD@//$CONNECTION_STR <<SQL
+set echo on
+set feedback on
 whenever sqlerror exit failure rollback
 whenever oserror exit failure rollback
 
-cd examples
 @RunAllExamplesAsTests.sql
 
 exit
