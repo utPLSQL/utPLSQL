c9eae209fae465dd0b7b47d9e1fe05d23426acb0
pesse <derpesse@gmail.com>
2023-05-02 17:54:36 +0200

    improve template to use latest cli

--- development/template.env.sh
+++ development/template.env.sh
@@ -2,9 +2,9 @@
 
 export SQLCLI=sql # For sqlcl client
 #export SQLCLI=sqlplus # For sqlplus client
-export CONNECTION_STR=127.0.0.1:1521/xe         ORACLE_VERSION=11g-r2-xe# Adjust the connect string
+export CONNECTION_STR=127.0.0.1:1521/xe # Adjust the connect string
 export ORACLE_PWD=oracle # Adjust your local SYS password
-export UTPLSQL_CLI_VERSION="3.1.6"
+export UTPLSQL_CLI_VERSION="3.1.9"
 export SELFTESTING_BRANCH=develop
 
 export UTPLSQL_DIR="utPLSQL_latest_release"
